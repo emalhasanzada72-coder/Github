60124351f4d7bbcbcaebc594245afe4f4f9d1580
emalhasanzada72-coder <emalhasanzada72@gmail.com>
2025-12-25 22:48:41 +0430

    secon project

---
+++ 2number git.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+    int num1, num2, sum;
+    printf("Enter 2 number:>\n");
+    scanf("%d %d", &num2, &num2);
+    sum = num1 + num2;
+    printf("This is my sum number %d\n", sum);
+    return 0;
+}
