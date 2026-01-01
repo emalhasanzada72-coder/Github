4bcc2911a213634461eaba30fa64b7877fe0e514
emalhasanzada72-coder <emalhasanzada72@gmail.com>
2026-01-01 19:41:28 +0430

    project

---
+++ arry.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <windows.h>
+
+int main() {
+int arr[4];
+for (int i = 0; i < 4; i++){
+    printf("یک نمبر وارد کنید که آرایه باشد ");
+printf("Enter 4 number:)");
+scanf("%d", &arr[4]);
+printf("%d\n");
+}
+
+    
+ return 0;
+}
