bd23ee77b2c0702964787e38bf5db55a400243f3
emalhasanzada72-coder <emalhasanzada72@gmail.com>
2026-01-03 01:12:27 +0430

    characters ond  float number

---
+++ character and float.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main( ) {
+    float a, b, zarb;
+    char x;
+    printf("Enter to float number :\n Enter character name :🙃\n");
+    scanf("%f %f", &a, &b);
+    scanf(" %c", &x);
+    zarb = a * b;
+    printf("my zarb number is %f\n   character name is %c\n 😜", zarb, x );
+    return 0; 
+
+}
