e7f43e6eff1d97a1441bf5248909a1a092306270
emalhasanzada72-coder <emalhasanzada72@gmail.com>
2026-01-02 22:49:24 +0430

    my firs while

--- sum.c
+++ sum.c
@@ -1,26 +1,13 @@
 #include <stdio.h>
 int main() {
-    int num1, num2, sum;
-    int cuont = 3;
-  printf("how mani time do you want to repeat?");
-  scanf("%d", &cuont);
-    for (int i = 0; i < cuont; i++) {
-       
-   
-    printf("Enter 2 number\n ");
-    
-    scanf("%d %d", &num1, &num2);
-   
+   int number = 1; 
+   printf("Enter a number (Enter 0 to stop):\n");
+   while (number != 0) {
+    scanf("%d", &number );
+    printf("You Entered %d\n", number );
 
-    sum = num1 + num2;
-    if (sum > 100){
-        printf("oh my good this is very big\n");
-    }else{
-        printf(" this is oky\n");
-    }
-    printf("His is my sum number %d\n", sum);
-    
-}
+   }
+   printf(" Program finishide ");
     return 0;
 
 }
