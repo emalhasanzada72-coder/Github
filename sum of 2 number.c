bbafe6a25dd0b93bd4b40d90a1b86e3070891d26
emalhasanzada72-coder <emalhasanzada72@gmail.com>
2026-01-03 11:23:31 +0430

    sum of tow number

---
+++ sum of 2 number.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+    int num1, num2, sum;
+    printf("Enter tow number >:)\n");
+    scanf("%d %d", &num1, &num2);
+    sum = num1 + num2;
+    printf("this is my sum number >:) %d\n", sum);
+    return 0;
+}
